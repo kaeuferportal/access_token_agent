8668865bfe3bee2f9da50a4ee80e4e22f667ff79
Hagen Mahnke <hagen.mahnke@kaeuferportal.de>
2016-05-03 14:12:47 +0200

    Remove obsolete Credentials class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.0.1
+
+- Remove obsolete class Credentials
+
 ## 2.0.0
 
 - Rename base_uri parameter to host
--- lib/access_token_agent.rb
+++ lib/access_token_agent.rb
@@ -1,5 +1,4 @@
 require 'access_token_agent/error'
 require 'access_token_agent/unauthorized_error'
 require 'access_token_agent/token'
-require 'access_token_agent/credentials'
 require 'access_token_agent/connector'
